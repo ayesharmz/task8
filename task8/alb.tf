bc09c5e916238609408aa0f68dcb1a5692a2ab01
ayesharmz <119579227+ayesharmz@users.noreply.github.com>
2025-06-19 15:58:13 +0530

    Update alb.tf

--- task8/alb.tf
+++ task8/alb.tf
@@ -1,28 +1,33 @@
+# alb.tf
+
 resource "aws_lb" "strapi" {
   name               = "strapi-alb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = module.vpc.public_subnets
+  subnets            = data.aws_subnets.default.ids
+
+  enable_deletion_protection = false
 }
 
 resource "aws_lb_target_group" "strapi" {
   name     = "strapi-tg"
   port     = 1337
   protocol = "HTTP"
-  vpc_id   = module.vpc.vpc_id
+  vpc_id   = data.aws_vpc.default.id
 
   health_check {
     path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200-399"
     interval            = 30
     timeout             = 5
     healthy_threshold   = 2
     unhealthy_threshold = 2
-    matcher             = "200"
   }
 }
 
-resource "aws_lb_listener" "strapi" {
+resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.strapi.arn
   port              = 80
   protocol          = "HTTP"
