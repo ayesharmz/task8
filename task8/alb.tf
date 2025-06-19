a81c930fb90bcf1ac148a7742bcece5b8ff811f5
ayesharmz <119579227+ayesharmz@users.noreply.github.com>
2025-06-19 16:11:58 +0530

    Update alb.tf

--- task8/alb.tf
+++ task8/alb.tf
@@ -1,21 +1,34 @@
-resource "aws_security_group" "ecs_tasks" {
-  name        = "ecs-tasks-sg"
-  description = "Allow ECS tasks to receive traffic from ALB"
-  vpc_id      = data.aws_vpc.default.id
+resource "aws_lb" "strapi" {
+  name               = "strapi-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = module.vpc.public_subnets
+}
 
-  ingress {
-    description = "Allow incoming traffic from ALB on port 1337"
-    from_port   = 1337
-    to_port     = 1337
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+resource "aws_lb_target_group" "strapi" {
+  name     = "strapi-tg"
+  port     = 1337
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
   }
+}
 
-  egress {
-    description = "Allow all outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+resource "aws_lb_listener" "strapi" {
+  load_balancer_arn = aws_lb.strapi.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.strapi.arn
   }
 }
