07af05ae907597e901382228a2a1a791761818e6
ayesharmz <119579227+ayesharmz@users.noreply.github.com>
2025-06-19 16:16:01 +0530

    Update ecs-service.tf

--- task8/ecs-service.tf
+++ task8/ecs-service.tf
@@ -3,12 +3,13 @@ resource "aws_ecs_service" "strapi" {
   cluster         = aws_ecs_cluster.strapi.id
   task_definition = aws_ecs_task_definition.strapi.arn
   launch_type     = "FARGATE"
-  desired_count   = 1
+
+  desired_count = 1
 
   network_configuration {
-    subnets          = data.aws_subnets.default.ids
+    subnets         = module.vpc.public_subnets
+    security_groups = [aws_security_group.ecs_sg.id]
     assign_public_ip = true
-    security_groups  = [aws_security_group.ecs_tasks.id]
   }
 
   load_balancer {
